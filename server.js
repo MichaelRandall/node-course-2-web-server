7d08896a0bb7084a695616b7a7e784caab249622
m_ran66 <m_ran66@hotmail.com>
2017-05-23 19:19:37 -0700

    add api call from express and node

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const request = require('request');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
 //express' set uses key value pairs to
 hbs.registerPartials(__dirname + '/views/partials')
+//tells express what view engine to use
 app.set('view engine', 'hbs');
 
-//app.use is called in the order that they in the file
+
 
 //use registers middleware - this logs request data to screen
 app.use((request, response, next) => {
@@ -17,10 +19,16 @@ app.use((request, response, next) => {
   var log = `${now}: ${request.method} ${request.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err) =>{
+    if(err){
+      console.log("trouble follows you");
+    }
+  });
   next(); //middleware needs to call next when it is done so app loads
 });
 
+//uncomment to intercept all requests since next is not called
+//nothing after this is executed
 // app.use((request, response, next) => {
 //   response.render('maintenance.hbs');
 // });
@@ -28,10 +36,12 @@ app.use((request, response, next) => {
 //use registers middleware - this serves up a directory
 app.use(express.static(__dirname + '/public'));
 
+//hbs helpers
 hbs.registerHelper('getCurrentYear', ()=>{
   return new Date().getFullYear();
 });
 
+//hbs helpers
 hbs.registerHelper('screamIt', (text)=>{
   return text.toUpperCase();
 })
@@ -71,6 +81,13 @@ app.get('/projects', (request, response) => {
   response.render('projects.hbs',{
     pageTitle: 'Projects Page',
   });
+});
+
+app.get('/zoom', (req, res) =>{
+  var options = {
+    url:'https://api.zoomcare.com/zoomapi-service/v2/rest/content/type/event/'
+  };
+  request(options).pipe(res);
 });
 
 app.listen(port, ()=> {
